d7cfab3fc5d767b637ea9a18b157baca05783fde
Jingwei Huang <Jingwei.Huang@auspost.com.au>
2016-04-27 06:11:16 +1000

    Implement hash table with collision handling

---
+++ FGDataStructure/FGDataStructure/HashTable/FGHashTableNode.h
@@ -0,0 +1,13 @@
+//
+//  FGHashTableNode.h
+//  FGDataStructure
+//
+//  Created by Jingwei Huang on 26/04/2016.
+//  Copyright © 2016 Feng Guo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FGHashTableNode : NSObject
+
+@end
---
+++ FGDataStructure/FGDataStructure/HashTable/FGHashTableNode.m
@@ -0,0 +1,13 @@
+//
+//  FGHashTableNode.m
+//  FGDataStructure
+//
+//  Created by Jingwei Huang on 26/04/2016.
+//  Copyright © 2016 Feng Guo. All rights reserved.
+//
+
+#import "FGHashTableNode.h"
+
+@implementation FGHashTableNode
+
+@end
