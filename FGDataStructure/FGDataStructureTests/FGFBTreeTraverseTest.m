486f5a9dbb30df2923e3e53d1a27a181d71bf87c
Stephen Zhang <yuanpeng.zhang@nab.com.au>
2016-12-11 14:05:32 +1100

    add tree traverse unit test

---
+++ FGDataStructure/FGDataStructureTests/FGFBTreeTraverseTest.m
@@ -0,0 +1,40 @@
+//
+//  FGFBTreeTraverseTest.m
+//  FGDataStructure
+//
+//  Created by Stephen Zhang on 10/12/2016.
+//  Copyright © 2016 Feng Guo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "FGFBTraverseTree.h"
+
+@interface FGFBTreeTraverseTest : XCTestCase
+
+@end
+
+@implementation FGFBTreeTraverseTest
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testInOrderTreeBuilder{
+    FGFBInOrderTreeBuilder *builder = [[FGFBInOrderTreeBuilder alloc] init];
+    FGFBTreeNode *root = [builder buildTreeWithInOrderSequence:@[@4, @5, @6, @9, @1, @3, @2]];
+    XCTAssertTrue([root.value isEqualToNumber:@9]);
+    XCTAssertTrue([root.left.value isEqualToNumber:@5]);
+    XCTAssertTrue([root.left.left.value isEqualToNumber:@4]);
+    XCTAssertTrue([root.left.right.value isEqualToNumber:@6]);
+    XCTAssertTrue([root.right.value isEqualToNumber:@3]);
+    XCTAssertTrue([root.right.left.value isEqualToNumber:@1]);
+    XCTAssertTrue([root.right.right.value isEqualToNumber:@2]);
+}
+
+@end
